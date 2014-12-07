23a3f7dddd35a3162d3f6707db9698a01fe0412a
Itay Inbar <inbaritay@gmail.com>
2014-12-07 21:43:39 +0200

    much better

--- PlayerSDK/src/com/kaltura/playersdk/KPPlayerConfig.java
+++ PlayerSDK/src/com/kaltura/playersdk/KPPlayerConfig.java
@@ -5,28 +5,42 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class KPPlayerConfig {
-	//TODO: maybe change string constants to Enum of known flashvars types
-	public final static String KP_PLAYER_CONFIG_NATIVE_AD_ID_KEY = "nativeAdId";
-	public final static String KP_PLAYER_CONFIG_NATIVE_CALL_OUT_KEY = "nativeCallout";
-	public final static String KP_PLAYER_CONFIG_CHROMECAST_KEY = "chromecast.plugin";
-	public final static String KP_PLAYER_CONFIG_LEAD_ANDROID_HLS = "Kaltura.LeadHLSOnAndroid";
 	
-	private Map<String,String> mFlashvarsDict;
+	public static enum Key{
+		KP_PLAYER_CONFIG_NATIVE_AD_ID_KEY("nativeAdId"),
+		KP_PLAYER_CONFIG_NATIVE_CALL_OUT_KEY("nativeCallout"),
+		KP_PLAYER_CONFIG_CHROMECAST_KEY("chromecast.plugin"),
+		KP_PLAYER_CONFIG_LEAD_ANDROID_HLS("Kaltura.LeadHLSOnAndroid");
+		
+		private String label;
+		
+		private Key(String str) {
+			this.label = str;
+		}
+		
+		public String toString() {
+			return this.label;
+		}
+		
+	}
+	
+	private Map<Key,String> mFlashvarsDict;
 	
 	public KPPlayerConfig(){
 		setupKPPlayerConfig();
 	}
 	
-	public void setConfigKey(String key, String value){
+	public void setConfigKey(Key key, String value){
 		mFlashvarsDict.put(key, value);
 	}
 	
 	public List<Map.Entry<String,String>> getFlashVarsArray(){
 		List<Map.Entry<String,String>> flashVarsArray = new ArrayList<Map.Entry<String,String>>();
-		for (Map.Entry<String,String> pairs : mFlashvarsDict.entrySet()) {
-			if(!pairs.getKey().isEmpty() && !pairs.getValue().isEmpty()) {
+		for (Entry<Key, String> pairs : mFlashvarsDict.entrySet()) {
+			if(pairs.getKey() != null && !pairs.getValue().isEmpty()) {
 				flashVarsArray.add(new AbstractMap.SimpleEntry<String, String>(String.format("flashvars[%s]",pairs.getKey()), pairs.getValue()));
 			}
 			
@@ -37,11 +51,11 @@ public class KPPlayerConfig {
 	
 	private void addDefaultFlags(){
 		if (mFlashvarsDict != null){
-			mFlashvarsDict.put(KP_PLAYER_CONFIG_NATIVE_AD_ID_KEY, ""); //TODO:define value for advertiser id
+			mFlashvarsDict.put(Key.KP_PLAYER_CONFIG_NATIVE_AD_ID_KEY, ""); //TODO:define value for advertiser id
 		}
 	}
 	private void setupKPPlayerConfig(){
-		mFlashvarsDict = new HashMap<String,String>();
+		mFlashvarsDict = new HashMap<Key,String>();
 		addDefaultFlags();
 	}
 }
