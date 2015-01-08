9e7a09ec056680634a0d39c03a51d3aad1f97228
Itay Inbar <itayi@Itays-MacBook-Pro.local>
2015-01-08 15:09:28 +0200

    FEC-2552 #comment trigger durationChange after setVideoUrl is called like the JS layer expect #time 30m

--- playerSDK/src/main/java/com/kaltura/playersdk/KalturaPlayer.java
+++ playerSDK/src/main/java/com/kaltura/playersdk/KalturaPlayer.java
@@ -144,8 +144,10 @@ public class KalturaPlayer extends FrameLayout implements ExoPlayer.Listener, Me
 		if ( !url.equals(mVideoUrl) ) {
 			releasePlayer();
 			mVideoUrl = url;
-            if ( mPlayheadUpdateListener != null )
-                mPlayheadUpdateListener.onPlayheadUpdated( 0 );
+            if ( mPlayheadUpdateListener != null ) {
+                mPlayheadUpdateListener.onPlayheadUpdated(0);
+            }
+            preparePlayer();
 		}		
 	}
 
