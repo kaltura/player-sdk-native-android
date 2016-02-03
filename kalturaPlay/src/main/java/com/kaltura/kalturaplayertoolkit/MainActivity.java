9a07cf1939a737fa4fecccbd649abb6e4aa6f44b
Noam Tamim <noamtamim@users.noreply.github.com>
2016-02-03 23:08:20 +0200

    Fixed demo url for kaltura play
    Must be on kgit master.

--- kalturaPlay/src/main/java/com/kaltura/kalturaplayertoolkit/MainActivity.java
+++ kalturaPlay/src/main/java/com/kaltura/kalturaplayertoolkit/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends Activity implements LoginFragment.OnFragmentIn
             extras = new Bundle();
         }
         
-        KPPlayerConfig config = new KPPlayerConfig("http://cdnapi.kaltura.com", "20540612", "243342").setEntryId("1_sf5ovm7u");
+        KPPlayerConfig config = new KPPlayerConfig("http://kgit.html5video.org/branches/master/mwEmbedFrame.php", "20540612", "243342").setEntryId("1_sf5ovm7u");
 
 
         config.setCacheSize(0.8f);
