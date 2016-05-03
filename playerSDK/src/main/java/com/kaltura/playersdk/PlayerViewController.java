f84424d9b4427e7a4c9a5db90016f49b810abeff
Noam Tamim <noam.tamim@kaltura.com>
2016-05-03 11:53:20 +0300

    PlayerViewController: AVOID lazy initialization of `mPlayerEventsHash` to prevent race conditions.

--- playerSDK/src/main/java/com/kaltura/playersdk/PlayerViewController.java
+++ playerSDK/src/main/java/com/kaltura/playersdk/PlayerViewController.java
@@ -72,7 +72,7 @@ public class PlayerViewController extends RelativeLayout implements KControlsVie
 
     private boolean mIsJsCallReadyRegistration = false;
     private Set<ReadyEventListener> mCallBackReadyRegistrations;
-    private HashMap<String, ArrayList<HashMap<String, EventListener>>> mPlayerEventsHash;
+    private final HashMap<String, ArrayList<HashMap<String, EventListener>>> mPlayerEventsHash = new HashMap<>();
     private HashMap<String, EvaluateListener> mPlayerEvaluatedHash;
     private Set<KPEventListener> eventListeners;
     private SourceURLProvider mCustomSourceURLProvider;
@@ -635,7 +635,7 @@ public class PlayerViewController extends RelativeLayout implements KControlsVie
                     bridgeMethod.invoke(object, args);
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                Log.e(TAG, "Error calling bridgeMethod " + bridgeMethod, e);
             }
         }
     }
@@ -708,9 +708,6 @@ public class PlayerViewController extends RelativeLayout implements KControlsVie
         this.registerReadyEvent(new ReadyEventListener() {
             @Override
             public void handler() {
-                if (mPlayerEventsHash == null) {
-                    mPlayerEventsHash = new HashMap();
-                }
                 ArrayList<HashMap<String, EventListener>> listenerArr = (ArrayList)mPlayerEventsHash.get(event);
                 if (listenerArr == null) {
                     listenerArr = new ArrayList();
