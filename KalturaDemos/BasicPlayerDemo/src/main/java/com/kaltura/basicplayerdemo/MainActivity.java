34b4795ae927772085612c9609d99f839ef341f0
Gilad Nadav <Gilad.Nadav@kaltura.com>
2016-05-04 18:14:00 +0300

    change to nwEmbed to work with latest tag and not localhost

--- KalturaDemos/BasicPlayerDemo/src/main/java/com/kaltura/basicplayerdemo/MainActivity.java
+++ KalturaDemos/BasicPlayerDemo/src/main/java/com/kaltura/basicplayerdemo/MainActivity.java
@@ -69,7 +69,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (mPlayer == null) {
             mPlayer = (PlayerViewController)findViewById(R.id.player);
             mPlayer.loadPlayerIntoActivity(this);
-            KPPlayerConfig config = new KPPlayerConfig("http://192.168.161.180/html5.kaltura/mwEmbed/mwEmbedFrame.php", "31638861", "1831271").setEntryId("1_ng282arr");
+
+            KPPlayerConfig config = new KPPlayerConfig("http://kgit.html5video.org/tags/v2.43.rc11/mwEmbedFrame.php", "31638861", "1831271").setEntryId("1_ng282arr");
             config.addConfig("chromecast.plugin", "true");
             config.addConfig("chromecast.applicationID", "5247861F");
             config.addConfig("chromecast.useKalturaPlayer", "true");
