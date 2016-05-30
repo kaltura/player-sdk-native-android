239f5696c643add355d5240792b8459e491d2cf2
Gilad Nadav <Gilad.Nadav@kaltura.com>
2016-05-30 08:50:11 +0300

    prevent autoplay in background

--- playerSDK/src/main/java/com/kaltura/playersdk/players/KPlayerController.java
+++ playerSDK/src/main/java/com/kaltura/playersdk/players/KPlayerController.java
@@ -121,6 +121,10 @@ public class KPlayerController implements KPlayerCallback, ContentProgressProvid
         }
         if (!isCasting) {
             player.play();
+            if (isBackgrounded){
+                player.pause();
+                isPlaying = true;
+            }
         } else {
             castPlayer.play();
         }
