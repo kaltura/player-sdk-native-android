504cb4fdcf6606a73cefa0256734c529e2b09850
Nissim Pardo <nissopa@gmail.com>
2016-07-11 22:45:02 +0300

    #FEM-607 #comment parse source from kaltura's receiver

--- playerSDK/src/main/java/com/kaltura/playersdk/casting/KCastKalturaChannel.java
+++ playerSDK/src/main/java/com/kaltura/playersdk/casting/KCastKalturaChannel.java
@@ -5,6 +5,9 @@ import android.util.Log;
 import com.google.android.gms.cast.Cast;
 import com.google.android.gms.cast.CastDevice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by nissimpardo on 07/12/15.
  */
@@ -13,7 +16,7 @@ public class KCastKalturaChannel implements Cast.MessageReceivedCallback {
     private KCastKalturaChannelListener mListener;
 
     public interface KCastKalturaChannelListener {
-        void readyForMedia();
+        void readyForMedia(String[] castParams);
     }
 
     public KCastKalturaChannel(String nameSpace, KCastKalturaChannelListener listener) {
@@ -28,8 +31,11 @@ public class KCastKalturaChannel implements Cast.MessageReceivedCallback {
     @Override
     public void onMessageReceived(CastDevice castDevice, String s, String s1) {
         Log.d(getClass().getSimpleName(), s + " " + s1);
-        if (s1.equals("readyForMedia")) {
-            mListener.readyForMedia();
+        if (s1.startsWith("readyForMedia")) {
+            String[] params = s1.split("\\|");
+            if (params.length == 3) {
+                mListener.readyForMedia(Arrays.copyOfRange(params, 1, 3));
+            }
         }
     }
 }
