8c8a7d57a7ab08cb9205c60e765853a74835d7ff
Gilad Nadav <Gilad.Nadav@kaltura.com>
2016-08-10 14:28:43 +0300

    Fix SDK side for pause come and goes in the web

--- playerSDK/src/main/java/com/kaltura/playersdk/players/KPlayerController.java
+++ playerSDK/src/main/java/com/kaltura/playersdk/players/KPlayerController.java
@@ -145,6 +145,9 @@ public class KPlayerController implements KPlayerCallback, ContentProgressProvid
     }
 
     public void setCastProvider(final KCastProvider castProvider) {
+        final boolean isPlayingBeforeCast = player.isPlaying();
+
+        player.pause();
         mCastProvider = (KCastProviderImpl)castProvider;
         mCastProvider.setInternalListener(new KCastProviderImpl.InternalListener() {
             @Override
@@ -180,14 +183,18 @@ public class KPlayerController implements KPlayerCallback, ContentProgressProvid
                 }
                 switch (state) {
                     case Loaded:
-                        boolean isPlayingBeforeCast = player.isPlaying();
-                        player.pause();
                         playerListener.eventWithValue(player, "hideConnectingMessage", null);
                         playerListener.eventWithValue(player, KPlayerListener.DurationChangedKey, Float.toString(getDuration() / 1000f));
                         playerListener.eventWithValue(player, KPlayerListener.LoadedMetaDataKey, "");
                         playerListener.eventWithValue(player, KPlayerListener.CanPlayKey, null);
+
                         if (isPlayingBeforeCast) {
                             playerListener.eventWithValue(player, KPlayerListener.PlayKey, null);
+                        } else {
+                            if (mCastProvider.getCastMediaRemoteControl() != null) {
+                                mCastProvider.getCastMediaRemoteControl().pause();
+                            }
+                            playerListener.eventWithValue(player, KPlayerListener.PauseKey, null);
                         }
                         break;
                     case Playing:
@@ -633,8 +640,6 @@ public class KPlayerController implements KPlayerCallback, ContentProgressProvid
 
     @Override
     public void playerStateChanged(int state) {
-
-        //LOGE(TAG, "XXXX playerStateChanged " + state);
         switch (state) {
             case KPlayerCallback.CAN_PLAY:
                 LOGD(TAG, "playerStateChanged CAN_PLAY");
