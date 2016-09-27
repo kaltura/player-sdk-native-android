b2b4428a5b18265f17934aafa1b517535d0c973d
Gilad Nadav <Gilad.Nadav@kaltura.com>
2016-09-27 13:50:13 +0300

    example for changing the CC format

--- KalturaDemos/CCPlayerDemo/src/main/java/com/kaltura/ccplayerdemo/MainActivity.java
+++ KalturaDemos/CCPlayerDemo/src/main/java/com/kaltura/ccplayerdemo/MainActivity.java
@@ -278,11 +278,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 config.addConfig("chromecast.applicationID", getString(R.string.app_id));
                 config.addConfig("chromecast.useKalturaPlayer", "true");
                 config.addConfig("chromecast.receiverLogo", "true");
+
                 //config.addConfig("chromecast.defaultThumbnail", "http://i.utdstc.com/icons/120/voot-android.png");
                 //config.addConfig("chromecast", "{\"proxyData\":" + proxyDataReceiver + "}");  // change media Format in order to stream it to TV in higher resolution
                 //config.addConfig("strings.mwe-chromecast-loading", "Loading to CC");  // Set Loading message
                 //config.addConfig("chromecast.logoUrl", "http://i.utdstc.com/icons/120/voot-android.png")
-
+                //config.addConfig("chromecast", "{\"proxyData\":" + proxyDataReceiver + "}");
 
 
                 //config.addConfig("topBarContainer.hover", "true");
