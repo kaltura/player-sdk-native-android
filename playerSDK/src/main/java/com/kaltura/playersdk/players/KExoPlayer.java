51cf7fc8c21040a3939f6cd3c47ae67843de74d0
sourav basu <sourav.basu@logituit.com>
2016-10-03 09:04:13 +0530

    voot-594. Handling specific error cases to call prepare.
    Signed-off-by: sourav basu <sourav.basu@logituit.com>

--- playerSDK/src/main/java/com/kaltura/playersdk/players/KExoPlayer.java
+++ playerSDK/src/main/java/com/kaltura/playersdk/players/KExoPlayer.java
@@ -533,6 +533,22 @@ public class KExoPlayer extends FrameLayout implements KPlayer, ExoplayerWrapper
                 errorString = "error_instantiating_decoder " +
                         decoderInitializationException.decoderName;
             }
+        } //[voot 594] 
+		else if (e.getCause() instanceof com.google.android.exoplayer.upstream.HttpDataSource.HttpDataSourceException) { // voot-723
+            mExoPlayer.prepare();
+            errorString = "HttpDataSourceException . Trying to recover";
+            LOGE(TAG, errorString);
+            return;
+        } else if ( e.getCause() instanceof java.net.UnknownHostException) {
+            mExoPlayer.prepare();
+            errorString = "UnknownHostException . Trying to recover";
+            LOGE(TAG, errorString);
+            return;
+        } else if ( e.getCause() instanceof java.net.ConnectException) {
+            mExoPlayer.prepare();
+            errorString = "ConnectException . Trying to recover";
+            LOGE(TAG, errorString);
+            return;
         }
         if (!"".equals(errorString)) {
             LOGE(TAG, errorString);
